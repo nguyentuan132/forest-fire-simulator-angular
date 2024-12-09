c7a0dd124420f9a355db2839a4ad2d75a17b4d67
nguyentuan132 <nguyentuan132@gmail.com>
2024-12-09 17:39:46 +0100

    autorun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ForestService } from './services/forest.service';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 import { Forest } from './model';
+import { ForestService } from './services/forest.service';
+import { MatCardModule } from '@angular/material/card';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, MatButtonModule, MatDividerModule, MatIconModule, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'forest-fire-simulator-angular';
+
+  autoRunning = false;
+  autoRunSubscription!: Subscription;
 
   simulationSteps: Forest[] = []; // Stores the steps of the simulation
   currentStep = 0; // Current step index in the simulation
@@ -25,7 +33,7 @@ export class AppComponent implements OnInit {
   }
 
   // Loads the simulation steps from the service
-  private loadSimulation(): void {
+   loadSimulation(): void {
     this.forestService.getSimulation().subscribe({
       next: (steps: Forest[]) => {
         this.simulationSteps = steps; // Save the simulation steps
@@ -61,4 +69,33 @@ export class AppComponent implements OnInit {
   isLastStep(): boolean {
     return this.currentStep === this.simulationSteps.length - 1; // Return true if we are at the last step
   }
+
+
+  toggleAutoRun(): void {
+    this.autoRunning = !this.autoRunning;
+    if (this.autoRunning) {
+      this.startAutoRun();
+    } else {
+      this.stopAutoRun();
+    }
+  }
+
+
+  private startAutoRun(): void {
+    this.autoRunSubscription = interval(600).subscribe(() => {
+      this.nextStep();
+      if (this.currentStep === this.simulationSteps.length - 1) {
+        this.stopAutoRun(); // Arrêter si on atteint la fin
+      }
+    });
+  }
+
+  private stopAutoRun(): void {
+    if (this.autoRunSubscription) {
+      this.autoRunSubscription.unsubscribe();
+    }
+    this.autoRunning = false;
+  }
 }
+
+
